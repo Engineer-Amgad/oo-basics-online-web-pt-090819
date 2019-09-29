c258cd7ccbb63f2efc60cf2a68470bc1e8aff0e5
Amgad Malik <amgadmalik@yahoo.com>
2019-09-29 02:35:34 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -3,7 +3,11 @@ class book
   attr_reader :title 
   
   def initialize(title)
-    
+    @title = title
+  end 
+  
+  def turn_page
+    Puts "Flipping the page...wow you read fast!"
   end 
 end 
 
